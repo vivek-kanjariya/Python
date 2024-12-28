d4d19609f0b8dd61303349f4823677434759693f
Vivek Kanjariya <144126963+vivek-kanjariya@users.noreply.github.com>
2024-12-28 16:32:53 +0530

    Add files via upload

---
+++ Day-1/Day1.py
@@ -0,0 +1,123 @@
+# ⁡⁢⁢⁢​‌‌‍𝗽𝘆𝘁𝗵𝗼𝗻 𝗕𝗮𝘀𝗶𝗰𝘀 ​⁡𝗗𝗮𝘆 𝟭
+
+# ⁡⁣⁢⁣𝗧𝗼𝗽𝗶𝗰𝘀 𝗖𝗼𝘃𝗲𝗿𝗲𝗱 , 𝗕𝗮𝘀𝗶𝗰 𝗦𝘆𝗻𝘁𝗲𝘅
+#               , 𝗩𝗮𝗿𝗶𝗮𝗯𝗹𝗲𝘀
+#               , 𝗗𝗮𝘁𝗮 𝗧𝘆𝗽𝗲𝘀
+#               , 𝗢𝗽𝗲𝗿𝗮𝘁𝗼𝗿𝘀
+#               , 𝗖𝗼𝗺𝗺𝗲𝗻𝘁𝘀
+#               , 𝗜𝗗𝗘, 𝗣𝘆𝘁𝗵𝗼𝗻 𝟯⁡
+
+
+# ⁡⁢⁣⁣​‌‍‌# 𝗩𝗮𝗿𝗶𝗮𝗯𝗹𝗲𝘀 𝗔𝗻𝗱 𝗗𝗮𝘁𝗮 𝗧𝘆𝗽𝗲𝘀​⁡
+
+# ⁡⁣⁣⁢# ‍𝗧𝗵𝗶𝘀 𝗽𝗿𝗼𝗰𝗲𝘀𝘀 𝗼𝗳 𝗰𝗿𝗲𝗮𝘁𝗶𝗻𝗴 𝗮 𝘃𝗮𝗿𝗶𝗮𝗯𝗹𝗲 𝘁𝗼 𝘀𝘁𝗼𝗿𝗲 𝗮 𝘃𝗮𝗹𝘂𝗲 𝗶𝘀 𝗰𝗮𝗹𝗹𝗲𝗱 
+# # 𝗧𝗵𝗲 𝗽𝗿𝗼𝗰𝗲𝘀𝘀 𝗼𝗳 𝘀𝗲𝘁𝘁𝗶𝗻𝗴 𝗶𝘁𝘀 𝘃𝗮𝗹𝘂𝗲 𝗳𝗼𝗿 𝘁𝗵𝗲 𝗳𝗶𝗿𝘀𝘁 𝘁𝗶𝗺𝗲 𝗶𝘀 𝗰𝗮𝗹𝗹𝗲𝗱 𝗜𝗻𝗶𝘁𝗶𝗮𝗹𝗶𝘇𝗮𝘁𝗶𝗼𝗻.⁡
+
+
+# Numeric Types
+a = 10
+b = 10.5
+c = 2 + 3j
+
+# Sequence Types
+my_list = [1, 2, 3, "apple", 4.5]
+my_tuple = (1, 2, 3, "banana", 4.5)
+my_range = range(1, 10)
+
+# Text Type
+my_string = "Hello, World!"
+
+# Set Types
+my_set = {1, 2, 3, "apple", 4.5}
+my_frozenset = frozenset([1, 2, 3, "banana", 4.5])
+
+# Mapping Type
+my_dict = {"name": "John", "age": 30, "city": "New York"}
+
+# Boolean Type
+is_active = True
+is_deleted = False
+
+# Binary Types
+my_bytes = b"Hello"
+my_bytearray = bytearray(b"Hello")
+my_memoryview = memoryview(b"Hello")
+
+# None Type
+my_none = None
+
+
+#  ⁡⁣⁢⁣Rules for Python variable names:
+#  A variable name can only contain alphabets, numbers and underscores (ie. A-Z, a-z, 0-9, and _).
+# A variable name cannot start with a number.
+# A variable name cannot have spaces in between.
+# Variable names are case-sensitive (age, Age and AGE are three different variables).
+
+
+
+# ⁡⁢⁣⁣​‌‍‌𝗔𝗹𝗹 𝗧𝘆𝗽𝗲𝘀 𝗼𝗳 𝗢𝗽𝗲𝗿𝗮𝘁𝗼𝗿𝘀​⁡
+# Arithmetic Operators
+a = 10
+b = 5
+print(a + b)  # 15
+print(a - b)  # 5
+print(a * b)  # 50
+print(a / b)  # 2.0
+print(a // b)  # 2
+print(a % b)  # 0
+print(a ** b)  # 100000
+
+# Comparison Operators
+print(a ==b)  # False
+print(a != b)  # True
+print(a > b)  # True
+print(a < b)  # False
+print(a >= b)  # True
+print(a <= b)  # False
+
+# Assignment Operators
+a = 10
+
+a += 5
+print(a)  # 15
+a -= 5
+print(a)  # 10
+a *= 5
+print(a)  # 50
+a /= 5
+print(a)  # 10.0
+a //= 5
+print(a)  # 2.0
+a %= 5
+print(a)  # 2.0
+a **= 5
+print(a)  # 32.0
+
+# Logical Operators
+a = 10
+b = 5
+
+
+print(a > 5 and b < 10)  # True
+print(a > 5 or b > 10)  # True
+print(not(a > 5))  # False
+
+# Bitwise Operators
+a = 10  # 1010 in binary
+b = 4   # 0100 in binary
+print(a & b)  # 0 (0000 in binary)
+print(a | b)  # 14 (1110 in binary)
+print(a ^ b)  # 14 (1110 in binary)
+print(~a)  # -11 (1011 in binary)
+print(a << 2)  # 40 (101000 in binary)
+print(a >> 2)  # 2 (0010 in binary
+
+# Membership Operators
+print(3 in [1, 2, 3])  # True
+print(4 not in [1, 2, 3])  # True
+
+# Identity Operators
+a = 10
+b = 10
+print(a is b)  # True
+print(a is not b)  # False
